77f2df7a41882637145683bb1acf60bb04fddc24
Clare Liguori <liguori@amazon.com>
2020-02-02 12:20:20 -0500

    fix: resolve commit ID in integ test script

--- run_integ_test.sh
+++ run_integ_test.sh
@@ -7,7 +7,7 @@ ACTION_GIT_COMMIT_ID=`git rev-parse HEAD`
 
 # Update the integ test action workflow file with the latest action commit ID
 git checkout integ-tests
-sed -i 's|aws-actions/configure-aws-credentials@v1|aws-actions/configure-aws-credentials@$ACTION_GIT_COMMIT_ID|g' test-workflow.yml
+sed -i "s|aws-actions/configure-aws-credentials@v1|aws-actions/configure-aws-credentials@$ACTION_GIT_COMMIT_ID|g" test-workflow.yml
 mkdir -p .github/workflows
 cp test-workflow.yml .github/workflows
 git add .github/workflows
@@ -18,7 +18,7 @@ git push origin integ-tests
 
 # Validate that the action workflow succeeds
 # Exit codes: success = 0; failure = 1; pending = 2; no status = 3
-while hub ci-status; [ $? -ge 2 ] do
+while hub ci-status; [ $? -ge 2 ]; do
   echo "waiting for test workflow to complete..."
   sleep 5
 done
