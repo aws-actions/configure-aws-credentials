746c17326f4450748d2c1a28fd6e12bc4a061923
Tom Keller <kellertk@amazon.com>
2022-10-15 18:16:42 -0700

    chore!: remove old dist files
    TODO: Add dist generation into the build steps

--- dist/helpers.d.ts
+++
@@ -1,7 +0,0 @@
-import { STSClient } from '@aws-sdk/client-sts';
-export declare function getStsClient(region: string, agent?: string): STSClient;
-export declare function sanitizeGithubActor(actor: string): string;
-export declare function sanitizeGithubWorkflowName(name: string): string;
-export declare function errorMessage(error: unknown): string;
-export declare function isDefined<T>(i: T | undefined | null): i is T;
-export declare function retryAndBackoff<T>(fn: () => Promise<T>, isRetryable: boolean, retries?: number, maxRetries?: number, base?: number): Promise<T>;
