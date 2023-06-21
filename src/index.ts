2cee35f9a95b5a4eff614d995c2718165f600644
peterwoodworth <woodwoop@amazon.com>
2023-06-21 14:51:50 -0700

    feat: optional policy inputs when assuming role

--- src/index.ts
+++ src/index.ts
@@ -33,7 +33,7 @@ export async function run() {
     const managedSessionPoliciesInput = core.getMultilineInput('managed-session-policies', { required: false })
     const managedSessionPolicies: any[] = [];
     for (const managedSessionPolicy of managedSessionPoliciesInput) {
-      managedSessionPolicies.push({arn: managedSessionPolicy})
+      managedSessionPolicies.push(managedSessionPolicy)
     }
 
     // Logic to decide whether to attempt to use OIDC or not
