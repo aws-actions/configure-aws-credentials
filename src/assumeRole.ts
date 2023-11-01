a4d19f7acb8bf81670fbfe16a2f9bc1a765589a4
ponkio-o <29038315+ponkio-o@users.noreply.github.com>
2023-11-01 11:45:01 +0900

    fix: change logic to determine if using long-term credentials

--- src/assumeRole.ts
+++ src/assumeRole.ts
@@ -56,11 +56,17 @@ async function assumeRoleWithWebIdentityTokenFile(
 
 async function assumeRoleWithCredentials(params: AssumeRoleCommandInput, client: STSClient) {
   core.info('Assuming role with user credentials');
-  if (!process.env['AWS_SESSION_TOKEN'] && process.env['AWS_ACCESS_KEY_ID'] && process.env['AWS_SECRET_ACCESS_KEY']) {
-    core.warning(
-      'To avoid using long-term AWS credentials, please update your workflows to authenticate using OpenID Connect.' +
-        ' See https://s12d.com/gha-oidc-aws for more information.'
-    );
+  const accessKey = process.env['AWS_ACCESS_KEY_ID'];
+  // check determine if using long-term credentials
+  // IAM access key starts with 'AKIA' prefix.
+  // https://docs.aws.amazon.com/IAM/latest/UserGuide/reference_identifiers.html#identifiers-unique-ids
+  if (accessKey) {
+    if (accessKey.startsWith('AKIA')) {
+      core.warning(
+        'To avoid using long-term AWS credentials, please update your workflows to authenticate using OpenID Connect.' +
+          ' See https://s12d.com/gha-oidc-aws for more information.'
+      );
+    }
   }
   try {
     const creds = await client.send(new AssumeRoleCommand({ ...params }));
