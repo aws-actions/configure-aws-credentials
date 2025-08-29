36af75968da3e86108d56480890efb64b289e4e3
Michael Lehmann <lehmanmj@amazon.com>
2025-08-29 16:32:13 -0700

    chore: remove unnecessary sudo and reorganize test files

--- .github/integ_tests/fetch-token.js
+++ .github/integ_tests/fetch-token.js
@@ -1,5 +1,5 @@
 import core from "@actions/core";
-import fs from "fs";
+import fs from "fs/promises";
 
 async function getIDTokenAction() {
    const id_token = await core.getIDToken("sts.amazonaws.com");
@@ -7,6 +7,6 @@ async function getIDTokenAction() {
 }
 let idToken = await getIDTokenAction();
 
-fs.writeFile("integ_token.txt", idToken, (err) => {
+await fs.writeFile(".github/integ_tests/integ_token.txt", idToken, (err) => {
   if (err) throw err;
 });
